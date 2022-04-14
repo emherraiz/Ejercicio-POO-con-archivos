cc439cd0b54d8dc0b1c042545bbacfe4220b4c38
Enrique Marina <eherraiz@myuax.com>
2022-04-14 20:37:12 +0200

    leyendo csv

--- Clases/lista_de_diccionarios.py
+++ Clases/lista_de_diccionarios.py
@@ -2,5 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-df = open('calificaciones.csv', 'r')
+df = open('INTRODUCCION POO\Ejercicio-POO-con-archivos\Clases\calificaciones.csv', 'r')
+DF = pd.DataFrame(df)
+print(DF.head())
 
