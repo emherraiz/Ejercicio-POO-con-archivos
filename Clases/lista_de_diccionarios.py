975c8ddf30c3ee8a6a555346495781ba9dcc5694
Enrique Marina <eherraiz@myuax.com>
2022-04-15 18:21:41 +0200

    acabado nos queda, separarlo en clases

--- Clases/lista_de_diccionarios.py
+++ Clases/lista_de_diccionarios.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-from sklearn.impute import SimpleImputer
 df = open('INTRODUCCION POO\Ejercicio-POO-con-archivos\Clases\calificaciones.csv', 'r')
 df = pd.read_csv(df, sep = ';')
 df = df.sort_values(by = 'Apellidos')
@@ -17,5 +15,47 @@ for i in columnas[2:]:
 
 df[columnas[2:]] = df[columnas[2:]].astype(float)
 
+nota_final = list()
+nota_final_parcial_1 = list()
+nota_final_parcial_2 = list()
+nota_final_practicas = list()
+for i in range(len(df)):
+    if df.iloc[i, 3] > df.iloc[i, 5]:
+        nota_final_parcial_1_i = df.iloc[i, 3]
+    else:
+        nota_final_parcial_1_i = df.iloc[i, 5]
+    nota_final_parcial_1.append(nota_final_parcial_1_i)
+
+
+    if df.iloc[i, 4] > df.iloc[i, 6]:
+        nota_final_parcial_2_i = df.iloc[i, 4]
+    else:
+        nota_final_parcial_2_i = df.iloc[i, 6]
+    nota_final_parcial_2.append(nota_final_parcial_2_i)
+
+
+    if df.iloc[i, 7] > df.iloc[i, 8]:
+        nota_final_practica_i = df.iloc[i, 7]
+    else:
+        nota_final_practica_i = df.iloc[i, 8]
+    nota_final_practicas.append(nota_final_practica_i)
+
+df['Nota Parcial 1'] = nota_final_parcial_1
+df['Nota Parcial 2'] = nota_final_parcial_2
+df['Nota Practicas'] = nota_final_practicas
+df['Nota final'] = (df['Nota Parcial 1'] + df['Nota Parcial 2']) * .3 + df['Nota Practicas'] * .4
+
+print(df['Asistencia'][6])
+
+Aprobado = []
+for i in range(len(df)):
+    if df.iloc[i, 2] >= 75 and df.iloc[i, 9] >= 4 and df.iloc[i, 10] >= 4 and df.iloc[i, 11] >= 4 and df.iloc[i, 12] >= 5:
+        Aprobado.append('Aprobado')
+    else:
+        Aprobado.append('Suspende')
+
+df['Aprobado'] = Aprobado
+
 print(df)
-#df.loc[df['Parcial1'] < df['Ordinario1']]
+
+
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from lanzador import *
-if __name__ == "__main__":
+#from lanzador import *
+#if __name__ == "__main__":
+a = [4, 5, 6, 7, 8, 9, 10]
