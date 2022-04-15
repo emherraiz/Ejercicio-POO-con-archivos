715255e1ce81648214abdde5c8018adfb8a137b2
Enrique Marina <eherraiz@myuax.com>
2022-04-15 03:44:26 +0200

    probando con lmda

--- Clases/lista_de_diccionarios.py
+++ Clases/lista_de_diccionarios.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
-import os
-
+from sklearn.impute import SimpleImputer
 df = open('INTRODUCCION POO\Ejercicio-POO-con-archivos\Clases\calificaciones.csv', 'r')
 df = pd.read_csv(df, sep = ';')
 df = df.sort_values(by = 'Apellidos')
-print(df)
 
+df = df.fillna(0)
+columnas = df.columns.values
+
+df['Asistencia'] = df['Asistencia'].str.split('%').apply(lambda x: x.pop(0))
+print(df['Asistencia'])
+
+for i in columnas[:2]:
+    df[i] = df[i].str.split(',').apply(lambda x: '.'.join(x))
+
+#df = df[columnas[:2]] + df[columnas[2:]].astype(float)
+
+print(df)
+#df.loc[df['Parcial1'] < df['Ordinario1']]
