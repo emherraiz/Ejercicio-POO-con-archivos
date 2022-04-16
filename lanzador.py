847ce9416e8eae276bf64964ffc6997bc6ca43db
Enrique Marina <eherraiz@myuax.com>
2022-04-16 04:40:00 +0200

    liado con los str

--- lanzador.py
+++ lanzador.py
@@ -1,5 +1,9 @@
-from Clases.parte_3 import *
+from Clases import *
 import pandas as pd
 
 df = open('INTRODUCCION POO\Ejercicio-POO-con-archivos\Clases\calificaciones.csv', 'r')
 df = pd.read_csv(df, sep = ';')
+print(df)
+
+resultado = Parte_3(df)
+print(resultado)
