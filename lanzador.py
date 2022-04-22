4fb749d8de0e90b720f4c2bc88996ca416228e11
Enrique Marina <eherraiz@myuax.com>
2022-04-22 12:55:22 +0200

    ACABADO

--- lanzador.py
+++ lanzador.py
@@ -1,9 +1,8 @@
-from Clases import *
-import pandas as pd
+from Clases.parte_3 import *
 
+import pandas as pd
 df = open('INTRODUCCION POO\Ejercicio-POO-con-archivos\Clases\calificaciones.csv', 'r')
 df = pd.read_csv(df, sep = ';')
-print(df)
-
 resultado = Parte_3(df)
-print(resultado)
+Resultado = resultado.alumnos_suspensos()
+
